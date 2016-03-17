7e76309dc45c8523e70031caa139b51c5b2d69e0
Artur Puzio <cytadela88@gmail.com>
2016-03-17 12:27:15 +0100

    Zmieniono na example.config.py

---
+++ example.config.py
@@ -0,0 +1,8 @@
+#-*- coding: utf-8 -*-
+# ssl_cert_file = "web.pem"
+# ssl_key_file = "web.key"
+admin_login = "admin"
+admin_password = "secret"
+database_uri = 'sqlite:///baza.db'
+secret_key = "very_secret_key_askjdhaejkbdnaqlwmedxawmhe"
+ldap_host = "localhost"
